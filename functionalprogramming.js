5ed2337b9b20dcbd48342a74cc41f2c8906fb0be
Tyler Neustaedter <rtneust@gmail.com>
2015-09-09 10:44:37 -0500

    updates to use forEach() for the reduce() and map(), as well as using reduce() for the filter.

--- functionalprogramming.js
+++ functionalprogramming.js
@@ -36,11 +36,17 @@ function reduce(array, callback){
   return total
 
 }
-
+    
+//     var total = array[0]
+//     for( var i = 1; i < array.length; i++){
+//         total = callback(array[i], total)
+//     }
+//     return(total)
+// }
 // tests
 // ---
 console.assert(
-    reduce([1, 2, 3, 4], function(a, v){ return a*v }) //=== 24
+    reduce([1, 2, 3, 4], function(a, v){ return a*v }) === 24
 )
 
 // ----------------------------
@@ -49,20 +55,21 @@ console.assert(
 // ----------------------------
 
 function map(array, callback){
-
-    var newarray = []
-
-    for(var i = 0; i < array.length; i++){
-        newvalue = callback(array[i])
-        newarray.push(newvalue)
-        console.log(newarray)
-    }
-    return newarray
-}
+    var newarray = []  
+    
+    forEach(array, function(element){
+        newarray.push(callback(element, array))
+        return(newarray)
+    })
+   return(newarray)
+}  
 
 // tests
 // ---
+
 var squares = map([1, 2, 3, 4], function(v){ return v*v })
+console.log('here come squares')
+console.log(squares)
 console.assert(squares[0] === 1)
 console.assert(squares[1] === 4)
 console.assert(squares[2] === 9)
@@ -75,21 +82,35 @@ console.assert(squares[3] === 16)
 
 function filter(array, callback){
     // YOUR CODE HERE
-var total = array[0]
+// var total = array[0]
 var newarray = []
+var total = array[0]   
     
-    for(var i = 0; i < array.length; i ++){
-        total = callback(array[i], total)
-        if(total === true){
-            newarray.push(array[i])
-            console.log(newarray)
-        }
-    }   
+    reduce(array, function(element){
+        total = callback(element, total)
+            if(total === true){
+                newarray.push(element)
+            }
+        
+    })
+    return(newarray)
 }
+//     for(var i = 0; i < array.length; i ++){
+//         total = callback(array[i], total)
+//         if(total === true){
+//             newarray.push(array[i])
+//             console.log(newarray)
+//         }
+//     } 
+//     return(newarray)  
+// }
 
 // tests
 // ---
+
 var evens = filter([1, 2, 3, 4], function(v){ return v%2 === 0 })
+console.log('here comes evens')
+console.log(evens)
 console.assert(evens[0] === 2)
 console.assert(evens[1] === 4)
 
